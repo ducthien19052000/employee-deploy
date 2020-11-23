92be42a29bcd375dca24877471ce8152bc23ef2b
ducthien19052000 <ducthien19052000@gmail.com>
2020-11-23 18:01:10 +0700

    push lan 1

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Home from './components/Home';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import { PrivateRoute } from './PrivateRoute';
+
+
+
+
+function App() {
+  return (
+    
+         <HashRouter>
+        <Switch>
+       
+        <Route path="/" exact><Login/></Route>
+        <PrivateRoute path="/home"component={Home}/>
+        
+        </Switch>
+         
+      </HashRouter>
+     
+    
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+ import { Provider } from 'react-redux';
+ import store from './redux/store/configStore.js'
+ import { Auth0Provider } from '@auth0/auth0-react';
+
+
+ReactDOM.render(
+
+    <Auth0Provider domain="dev-4ikv6116.us.auth0.com" clientId="hqIvgYBOGNshJ4h2YFV3k04iSnwM9krw" redirectUri={window.location.origin}>
+    <Provider store = {store}>
+    <App />
+    </Provider>
+    </Auth0Provider>
+,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
